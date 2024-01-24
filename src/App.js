74df999f7d408ed3227f2f2fbdf7cd16a3e3d68a
Zehra Şeren <fatmazehraseren@gmail.com>
2024-01-24 19:52:22 +0300

    Listeleme ve sil aksiyonu yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import "./App.css";
 import CreateTask from "./CreateTask";
 import Header from "./Header";
+import { useState } from "react";
+import Task from "./Task";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   const addTask = (newTask) => {
-    console.log(newTask);
+    setTasks((prevTask) => {
+      return [...prevTask, newTask];
+    });
   };
+
+  const deleteTask = (id) => {
+    setTasks((prevTask) => {
+      return prevTask.filter((taskItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div className="App">
       <Header />
       <CreateTask onAdd={addTask} />
+      <div className="container">
+        <div className="row my-5">
+          {tasks.map((taskItem, index) => {
+            return (
+              <Task
+                key={index}
+                id={index}
+                title={taskItem.title}
+                content={taskItem.content}
+                onDelete={deleteTask}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
